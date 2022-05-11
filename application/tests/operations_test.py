52b4b1c7becfe1b729568d299b2ced5d3b1c5cbe
Cosmin070 <61682964+Cosmin070@users.noreply.github.com>
2022-05-11 09:39:14 +0300

    test sonarcloud integration part 4

--- application/tests/operations_test.py
+++ application/tests/operations_test.py
@@ -3,9 +3,9 @@ from application.operations import summation
 
 
 class OperationsTest(unittest.TestCase):
-    
+
     def test_summation(self):
          self.assertEqual('7', summation('3', '4'))
-        
+
     def test_summation_error(self):
         self.assertEqual('20', summation('7','13'))
