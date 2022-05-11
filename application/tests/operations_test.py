7f4420110cf8f75574d816de70215d11069fd916
Cosmin070 <61682964+Cosmin070@users.noreply.github.com>
2022-05-11 09:42:02 +0300

    Update operations_test.py

--- application/tests/operations_test.py
+++ application/tests/operations_test.py
@@ -1,8 +1,11 @@
-# import unittest
-# from application.operations import summation
+import unittest
+from application.operations import summation
 
 
-# class OperationsTest(unittest.TestCase):
-
-#     def test_summation(self):
-#         self.assertEqual('7', summation('3', '4'))
+class OperationsTest(unittest.TestCase):
+    
+    def test_summation(self):
+         self.assertEqual('7', summation('3', '4'))
+        
+    def test_summation_error(self):
+        self.assertEqual('20', summation('7','13'))
