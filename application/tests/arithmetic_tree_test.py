fe753bb2cd28e244f51b86e81467a4f08dad39a6
andreipatrauceanu <andrei.patrauceanu1@gmail.com>
2022-05-15 11:16:29 +0300

    first test for arithmetic tree

---
+++ application/tests/arithmetic_tree_test.py
@@ -0,0 +1,11 @@
+import unittest
+from application.arithmetic_tree import get_prio, get_result, compute_operation, check_next_char, check_close_parenthesis, check_parenthesis, evaluate
+
+
+class OperationsTest(unittest.TestCase):
+
+    def test_get_prio(self):
+        self.assertEqual('1', get_prio('+'))
+        self.assertEqual('2', get_prio('*'))
+        self.assertEqual('3', get_prio('^'))
+        self.assertEqual('0', get_prio(';'))
