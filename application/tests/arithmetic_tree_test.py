55eff2ea30ea902d50924e5cedec82cac60ab72e
andreipatrauceanu <andrei.patrauceanu1@gmail.com>
2022-05-15 12:53:22 +0300

    first try for get result test

--- application/tests/arithmetic_tree_test.py
+++ application/tests/arithmetic_tree_test.py
@@ -1,4 +1,6 @@
+import random
 import unittest
+from unittest.mock import Mock, patch
 from application.arithmetic_tree import get_prio, get_result, compute_operation, check_next_char, check_close_parenthesis, check_parenthesis, evaluate
 
 
@@ -9,3 +11,10 @@ class OperationsTest(unittest.TestCase):
         self.assertEqual(2, get_prio('*'))
         self.assertEqual(3, get_prio('^'))
         self.assertEqual(0, get_prio(';'))
+
+    @patch('application.arithmetic_tree.summation')
+    def test_get_result(self, get_content_mock):
+        get_content_mock.return_value = 'mocked'
+        self.assertEqual(get_result(), 'mocked')
+        self.assertEqual(get_content_mock.call_count, 1)
+        get_content_mock.assert_called_once()
